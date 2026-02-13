052d35242733fef70526b81f96baa8ad1bdcf03d
Youpileouf <amaury.kasprzyk@gmail.com>
2026-02-13 13:12:04 +0100

    Restore Trainer Fill Functions

--- src/event_object_movement.c
+++ src/event_object_movement.c
@@ -1188,7 +1188,7 @@ static void ClearObjectEvent(struct ObjectEvent *objectEvent)
     objectEvent->movementActionId = MOVEMENT_ACTION_NONE;
 }
 
-static void ClearAllObjectEvents(void)
+void ClearAllObjectEvents(void)
 {
     u8 i;
 
