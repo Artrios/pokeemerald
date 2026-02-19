707cf4208d0ea7da53ccd20150b589bc475ea5dd
Artrios <dapaquelet@hotmail.com>
2026-02-19 23:41:05 +0800

    Don't try to load a black palette in Trainer Hill

--- src/overworld.c
+++ src/overworld.c
@@ -1972,7 +1972,6 @@ static bool32 ReturnToFieldLocal(u8 *state)
         break;
     case 1:
         InitViewGraphics();
-        TryLoadTrainerHillEReaderPalette();
         (*state)++;
         break;
     case 2:
